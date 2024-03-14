6bde01623badfc5e52c9ed2da396fc17f8dda6e1
AUVADAIAPPAN S <130080437+Auvadaiappan@users.noreply.github.com>
2024-03-14 15:44:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Flutter Developer", "App Developer", "Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
